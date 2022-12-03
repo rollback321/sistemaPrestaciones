337ca30945b1e05b5e17ab3b04d79400b8bb8411
rollback321 <rd8995851@gmail.com>
2022-12-02 23:21:54 -0400

    sistema de prestaciones version 1

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- APP MAIN ==========-->
+    <div>
+                        <h1>-----Univ. :Ronald Lifonzo Laime</h1>
+                        <h2>-----C.I.: 10028172 LP</h2>
+                    </div>
+    <main id="app-main" class="app-main">
+        <div class="wrap">
+            <section class="app-content">
+                <div class="row">
+                    <div class="col-md-12 col-lg-8 offset-lg-2">
+                        <div class="widget">
+                            <header class="widget-header">
+                                <h4 class="widget-title">Agregar usuario</h4>
+                            </header><!-- .widget-header -->
+                            <hr class="widget-separator">
+                            <div class="widget-body">
+                                <form method="POST" action="{{url('admin/user')}}" enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                    <div class="form-group">
+                                        <label for="username">Username:</label>
+                                        <input type="text" name="username"   class="form-control" id="username" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="name">Nombre:</label>
+                                        <input type="text" name="name"  class="form-control" id="name" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="pwd">Contraseña:</label>
+                                        <input type="password" name="pwd"   class="form-control" id="pwd" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="level">Nivel:</label>
+                                        <select name="level"  class="form-control" id="level">
+                                            <option value="agent">Agente</option>
+                                            <option value="supervisor">Supervisor</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-success btn-block btn-md">Guardar</button>
+                                    </div>
+                                </form>
+
+                            </div><!-- .widget-body -->
+                        </div><!-- .widget -->
+                    </div><!-- END column -->
+                </div><!-- .row -->
+            </section>
+        </div>
+    </main>
+@endsection
